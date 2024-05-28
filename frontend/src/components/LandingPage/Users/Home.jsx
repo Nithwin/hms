b1393fa3f848eae36623f0fceb79bcffd010f298
Nithwin <luckyjoker2004@gmail.com>
2024-05-28 23:07:30 +0530

    AN

--- frontend/src/components/LandingPage/Users/Home.jsx
+++ frontend/src/components/LandingPage/Users/Home.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
 export default function Home(){
-  return(<div>
-hi
+  return(<section className=" bg-[#210643] h-screen m-2 rounded overflow-hidden flex">
+    <div className=" text-white">
+      <p>Hostel <span>Management</span> System</p>
+    </div>
+    <div>
 
-  </div>);
+    </div>
+  </section>);
 }
--- frontend/src/components/LandingPage/Users/NavBar.jsx
+++ frontend/src/components/LandingPage/Users/NavBar.jsx
@@ -1,8 +1,18 @@
 import React from "react";
-
+import { NavLink } from "react-router-dom";
+import logo from '../../images/book.png'
 export default function NavBar (){
   return(
-  <header className="bg-blue-900 ">
-    
-  </header>);
+    <nav className='flex justify-between font-bold text-white bg-[#020f39] px-10 py-4 items-center m-2 rounded-lg'>
+    <div className="flex items-center" >
+      <img src={logo} alt="book" className="me-2" />
+      <NavLink to="/" className="py-5 text-3xl">HMS</NavLink>
+    </div>
+    <div className="flex space-x-5 items-center text-xl ">
+      <NavLink to="/contact" className="px-5 py-3 rounded hover:bg-slate-50 hover:text-gray-950 transition-all ease-linear hover:delay-75" >Contact</NavLink>
+      <NavLink to="/about" className="px-5 py-3 rounded hover:bg-slate-50 hover:text-gray-950 transition-all ease-linear hover:delay-75">About</NavLink>
+      <NavLink to="/adim" className="px-5 py-3 rounded hover:bg-slate-50 hover:text-gray-950 transition-all ease-linear hover:delay-75" >Adim</NavLink>
+      <NavLink to="/login" className="px-5 py-3 bg-blue-600 rounded hover:bg-blue-800 transition-all ease-linear hover:delay-75" >Login</NavLink>
+    </div>
+  </nav>);
 }
